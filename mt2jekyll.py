fea9fd334035fcf8f385cdc13f89caf34facc297
Jack Moffitt <jack@metajack.im>
2008-12-22 10:02:45 -0700

    Added extended body support and fixed tags.

--- mt2jekyll.py
+++ mt2jekyll.py
@@ -6,10 +6,10 @@ import sys
 
 import mt
 
-
 def convert(mtfile, postpath):
     entries = mt.parse_export(mtfile)
     
+    # convert posts to jekyll format
     for e in entries:
 	if e['metadata']['status'] == 'Draft':
 	    continue
@@ -28,10 +28,18 @@ def convert(mtfile, postpath):
 	else:
 	    f.write('title: %s\n' % e['metadata']['title'])
 	if 'tags' in e['metadata']:
-	    f.write('tags: %s\n' % e['metadata']['tags'])
+	    f.write('tags: [%s]\n' % e['metadata']['tags'].replace(',', ', '))
+	f.write('time: "%s"\n' % e['metadata']['date'].strftime('%H:%M'))
+	if 'extended body' in e:
+	    f.write('extended: ":EXTENDED:"\n')
 	f.write('---\n\n')
 
 	f.write(e['body']['data'])
+
+	if 'extended body' in e:
+	    f.write('\n:EXTENDED:\n\n')
+	    f.write(e['extended body']['data'])
+
 	f.close()
 
 
